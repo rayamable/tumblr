0ca86a57ba149c61c82b7fc091f74a0c27aa8045
Ray Amable <rayamable@gmail.com>
2020-02-07 21:40:19 +0000

    add edit and delete posts

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,11 +1,11 @@
 class PostsController < ApplicationController
-
+    before_action :authenticate_user!, except: [:index, :show]
     def index
-        @posts = Post.all.order("created_at DESC") 
+        @posts = Post.all.order("created_at DESC")
     end
 
     def new
-        @post = Post.new    
+        @post = Post.new
     end
 
     def create
@@ -19,12 +19,34 @@ class PostsController < ApplicationController
     end
 
     def show
-       @post = Post.find(params[:id]) 
+        @post = Post.find(params[:id])
     end
 
-    private 
+    def edit
+        @post = Post.find(params[:id])
+    end
+
+    def update
+        @post = Post.find(params[:id])
+
+        if @post.update(params[:post].permit(:title, :body))
+            redirect_to @post
+        else
+            render 'edit'
+        end
+    end
+
+    def destroy
+        @post = Post.find(params[:id])
+        @post.destroy
+
+        redirect_to root_path
+    end
+
+    private
 
     def post_params
-        params.require(:post).permit(:title, :body)     
+        params.require(:post).permit(:title,:body)
     end
+
 end
